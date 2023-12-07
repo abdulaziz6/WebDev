97602aeeb3821bfe425dd31fd612c10076fb3db0
NebulaVII <66179846+NebulaVII@users.noreply.github.com>
2023-12-07 10:56:14 +0300

    console log test

--- example-app/public/js/imageSize.js
+++ example-app/public/js/imageSize.js
@@ -1,11 +1,13 @@
-
 // Function to toggle the size of the image
 function toggleImageSize(event) {
+    console.log(
+        "sasdsadassdsadsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    );
     var img = event.target;
-    img.classList.toggle('enlarged');
+    img.classList.toggle("enlarged");
 }
 
 // Add click event listener to each image
-document.querySelectorAll('.gallery-image').forEach(function(img) {
-    img.addEventListener('click', toggleImageSize);
+document.querySelectorAll(".gallery-image").forEach(function (img) {
+    img.addEventListener("click", toggleImageSize);
 });
