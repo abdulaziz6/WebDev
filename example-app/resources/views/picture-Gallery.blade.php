0909e4a5a549cd19f7e9ede704deb04184ce708c
Abdulaziz Alghamdi <abdulaziz0khadr@Hotmail.com>
2023-12-07 10:23:13 +0300

    fix js

--- example-app/resources/views/picture-Gallery.blade.php
+++ example-app/resources/views/picture-Gallery.blade.php
@@ -16,32 +16,32 @@
   <div class="gallery">
     <ul>
       <li>
-        <a href="#"><img src="{{ asset('image/main-logo.png') }}" width="500" height="500" alt=""></a>
+        <a href="#"><img src="{{ asset('image/main-logo.png') }}" width="500" height="500" alt="" class="gallery-image" id="image1"></a>
       </li>
       <li>
-        <a href="#"><img src="{{ asset('image/background1.jpg') }}" width="500" height="500" alt=""></a>
+        <a href="#"><img src="{{ asset('image/background1.jpg') }}" width="500" height="500" alt="" class="gallery-image" id="image2">></a>
       </li>
       <li>
-        <a href="#"><img src="{{ asset('image/card-background.jpg') }}" width="500" height="500" alt=""></a>
+        <a href="#"><img src="{{ asset('image/card-background.jpg') }}" width="500" height="500" alt="" class="gallery-image" id="image3">></a>
       </li>
       <li>
-        <a href="#"><img src="{{ asset('image/Jeddah.jpg') }}" width="500" height="500" alt=""></a>
+        <a href="#"><img src="{{ asset('image/Jeddah.jpg') }}" width="500" height="500" alt="" class="gallery-image" id="image4">></a>
       </li>
       <li>
-        <a href="#"><img src="{{ asset('image/footer-background.jpg') }}" width="500" height="500" alt=""></a>
+        <a href="#"><img src="{{ asset('image/footer-background.jpg') }}" width="500" height="500" alt="" class="gallery-image" id="image5">></a>
       </li>
       <li>
-        <a href="#"><img src="{{ asset('image/Plane.png') }}" width="500" height="500" alt=""></a>
+        <a href="#"><img src="{{ asset('image/Plane.png') }}" width="500" height="500" alt="" class="gallery-image" id="image6">></a>
       </li>
       <li>
-        <a href="#"><img src="{{ asset('image/Riyadh.jpg') }}" width="500" height="500" alt=""></a>
+        <a href="#"><img src="{{ asset('image/Riyadh.jpg') }}" width="500" height="500" alt="" class="gallery-image" id="image7">></a>
       </li>
       <li>
-        <a href="#"><img src="{{ asset('image/preview.jpg') }}" width="500" height="500" alt=""></a>
+        <a href="#"><img src="{{ asset('image/preview.jpg') }}" width="500" height="500" alt="" class="gallery-image" id="image8">></a>
       </li>
     </ul>
   </div>
-
+  
   @include('layouts.footer') <!-- Include the footer -->
 </body>
 </html>
