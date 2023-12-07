06267f63345dbc87ee13268f0161dbd746d58e7a
NebulaVII <66179846+NebulaVII@users.noreply.github.com>
2023-12-07 13:30:19 +0300

    fixed xhtml link

--- example-app/resources/views/layouts/footer.blade.php
+++ example-app/resources/views/layouts/footer.blade.php
@@ -4,8 +4,7 @@
   <p>&copy; 2023 TP. Travel Planner.</p>
   <div class="footer-container">
 
-        <a href="https://validator.w3.org/check?uri=http%3A%2F%2Flocalhost%3A3000%2F;accept=text%2Fhtml%2Capplication%2Fxhtml%2Bxml%2Capplication%2Fxml%3Bq%3D0.9%2Cimage%2Favif%2Cimage%2Fwebp%2Cimage%2Fapng%2C*%2F*%3Bq%3D0.8%2Capplication%2Fsigned-exchange%3Bv%3Db3%3Bq%3D0.7;accept-language=ar%2Cen-US%3Bq%3D0.9%2Cen%3Bq%3D0.8
-Validation Results - W3C Markup Validator" target="_blank">
+        <a href="http://validator.w3.org/check?uri=referer%22%3E" target="_blank">
             <img src="https://www.w3.org/Icons/valid-xhtml10" alt="Valid HTML 4.01" height="31" width="88">
         </a>
         <a href="https://jigsaw.w3.org/css-validator/check/referer" target="_blank">
